b9025cf757ba1a939e78c1e5c62ed39de76b8dc6
Keith <22567927+keithburner@users.noreply.github.com>
2020-01-06 17:37:01 +0000

    tested working version with inversion of a 8 sqaure matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
+## Part of R Programming module Week 3, this is the project assignment to create a square
+## matrix and its inverse. To then store this in cache and retrieve it from cache
+
 ## functions do
 
-## Write a short comment describing this function
+## Calculate the Inverse of a Square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    im <- NULL
+    set <- function(y) {
+        x <<- y
+        im <<- NULL
+    }
+    get <- function() x
+    setinv <- function(invmatrix) im <<- invmatrix
+    getinv <- function() im
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv
+         )
 }
 
-
-## Write a short comment describing this function
+## Use the cache to find the inverse of a Square Matrix already calculated, or calculate it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    invmatrix <- x$getinv()
+    if(!is.null(invmatrix)) {
+        message("getting cached data")
+        return(invmatrix)
+    }
+    data <- x$get()
+    invmatrix <- solve(data, ...)
+    x$setinv(invmatrix)
+    invmatrix
 }
